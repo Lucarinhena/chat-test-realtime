f968c3b5c6dc10a08851ab5c97c72f61f55a4521
Luca de Almeida Carinhena <luca.carinhena@falavinha.local>
2024-03-20 08:45:54 -0300

    feat: add vercel cfg

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,6 @@ const app = new App();
 
 dotenv.config();
 
-app.server.listen(process.env.PORT, () => {
+app.server.listen(process.env.PORT  || 3008, () => {
   console.log("Server Running at port " + process.env.PORT);
 });
